784a97b748405c99393496384fd3e8e7fdf1771c
hyoussef@ridgeco.tech <hyoussef@ridgeco.tech>
2025-03-19 01:52:04 +0300

    [UPDATE] update jsons files

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,23 +1,24 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-
 import commonEn from "./locales/en/common.json";
 import commonAr from "./locales/ar/common.json";
+import itemsEn from "./locales/en/items.json";
+import itemsAr from "./locales/ar/items.json";
 import { useLocaliztionStore } from "./store/useLocaliztionStore";
 
 // Get the initial language from Zustand
 const { language } = useLocaliztionStore.getState();
 
 const resources = {
-  en: { common: commonEn },
-  ar: { common: commonAr },
+  en: { common: commonEn, items: itemsEn },
+  ar: { common: commonAr, items: itemsAr },
 };
 
 i18n.use(initReactI18next).init({
   resources,
   lng: language, // Use Zustand language
   fallbackLng: "en",
-  ns: ["common"],
+  ns: ["common", "items"],
   defaultNS: "common",
   interpolation: { escapeValue: false },
 });
--- src/pages/products/Brands/brands.tsx
+++ src/pages/products/Brands/brands.tsx
@@ -4,21 +4,24 @@ import ComponentCard from "../../../components/common/ComponentCard";
 import PageBreadcrumb from "../../../components/common/PageBreadCrumb";
 import PageMeta from "../../../components/common/PageMeta";
 import { useFetchBrands } from "../../../hooks/prouducts/useBrands";
+import { useTranslation } from "react-i18next";
 
 export default function Brands() {
   const { data } = useFetchBrands();
   const brands: any = data || [];
 
+  const { t } = useTranslation("items");
+  
   return (
     <>
       <PageMeta
         title="Brands Management | Dashboard"
         description="Manage your product brands in the system."
       />
-      <PageBreadcrumb pageTitle="Brands" />
+      <PageBreadcrumb pageTitle={t("brands")} />
 
       <div className="space-y-4">
-        <ComponentCard title="Brand">
+        <ComponentCard title={t("brands")}>
           <DataTable
             columns={brandColumns}
             data={brands}
