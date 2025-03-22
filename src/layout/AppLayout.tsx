9467fec43afccc13fb193459c5af2198e36e3c60
hyoussef@ridgeco.tech <hyoussef@ridgeco.tech>
2025-03-23 00:19:41 +0300

    .

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,38 +1,46 @@
 import { useLocaliztionStore } from "@/store/useLocaliztionStore";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import { SidebarProvider, useSidebar } from "../context/SidebarContext";
 import AppHeader from "./AppHeader";
 import AppSidebar from "./AppSidebar";
 
 const LayoutContent: React.FC = () => {
-  const { isExpanded, isHovered, isMobileOpen } = useSidebar();
+  const { isExpanded, isHovered } = useSidebar();
   const { direction } = useLocaliztionStore();
+  const location = useLocation();
+
+  const isSettingsPage = location.pathname.startsWith("/settings");
 
   return (
     <div className="min-h-screen flex flex-col bg-[#FFFFFF]">
+      {/* الهيدر */}
       <div className="fixed top-0 left-0 w-full z-50 bg-white shadow">
         <AppHeader />
       </div>
 
       <div className="flex flex-1 pt-[49px]">
-        <div 
-          className={`fixed h-screen transition-all duration-300 z-40
-            ${isExpanded || isHovered ? "w-[184px]" : "w-[90px]"} 
-            ${direction === "rtl" ? "right-0" : "left-0"}`}
-        >
-          <AppSidebar />
-        </div>
-        
-        <div 
+        {!isSettingsPage && (
+          <div
+            className={`fixed h-screen transition-all duration-300 z-40
+              ${isExpanded || isHovered ? "w-[184px]" : "w-[90px]"} 
+              ${direction === "rtl" ? "right-0" : "left-0"}`}
+          >
+            <AppSidebar />
+          </div>
+        )}
+
+        <div
           className={`flex-1 transition-all duration-300 ease-in-out px-4
             ${
-              isExpanded || isHovered 
-                ? direction === "ltr" 
-                  ? "ml-[184px]" 
-                  : "mr-[210px]"
-                : direction === "ltr" 
-                  ? "ml-[90px]" 
+              !isSettingsPage
+                ? isExpanded || isHovered
+                  ? direction === "ltr"
+                    ? "ml-[184px]"
+                    : "mr-[184px]"
+                  : direction === "ltr"
+                  ? "ml-[90px]"
                   : "mr-[90px]"
+                : "ml-0"
             }`}
         >
           <Outlet />
