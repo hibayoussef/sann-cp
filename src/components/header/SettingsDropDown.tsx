c104836de0b9a56c934df33a5f2b7e3827805dbf
hyoussef@ridgeco.tech <hyoussef@ridgeco.tech>
2025-03-22 12:52:26 +0300

    [UPDATE] update app icons and settings

--- src/components/header/SettingsDropDown.tsx
+++ src/components/header/SettingsDropDown.tsx
@@ -1,8 +1,8 @@
+import { useLocaliztionStore } from "@/store/useLocaliztionStore";
 import { useState } from "react";
 import { useTheme } from "../../context/ThemeContext";
-import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 import Label from "../form/Label";
-import { useLocaliztionStore } from "@/store/useLocaliztionStore";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 
 const ToggleSwitcher = ({
   enabled,
@@ -41,23 +41,21 @@ const SettingsSidebar = () => {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 animate-spin duration-3000"
+          className="h-5 w-5"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
+          strokeWidth={2}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-          />
+          <circle cx="5" cy="5" r="1.5" fill="currentColor" />
+          <circle cx="12" cy="5" r="1.5" fill="currentColor" />
+          <circle cx="19" cy="5" r="1.5" fill="currentColor" />
+          <circle cx="5" cy="12" r="1.5" fill="currentColor" />
+          <circle cx="12" cy="12" r="1.5" fill="currentColor" />
+          <circle cx="19" cy="12" r="1.5" fill="currentColor" />
+          <circle cx="5" cy="19" r="1.5" fill="currentColor" />
+          <circle cx="12" cy="19" r="1.5" fill="currentColor" />
+          <circle cx="19" cy="19" r="1.5" fill="currentColor" />
         </svg>
       </button>
 
@@ -117,16 +115,6 @@ const SettingsSidebar = () => {
                   <Label htmlFor="rtl">RTL</Label>
                 </div>
               </RadioGroup>
-              {/* <div className="flex items-center gap-2">
-                <span className="text-gray-500 text-[13px] dark:text-gray-300">
-                  LTR
-                </span>
-              </div>
-              <div className="flex items-center gap-2">
-                <span className="text-gray-500 text-[13px] dark:text-gray-300">
-                  RTL
-                </span>
-              </div> */}
             </div>
           </div>
 
